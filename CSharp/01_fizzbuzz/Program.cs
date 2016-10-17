1b9da177cb41506b73f718e04fd038326961404b
fsheikh <farhanssheikh@gmail.com>
2016-10-17 16:15:05 -0500

    fizzbuzz complete

---
+++ CSharp/01_fizzbuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {   
+            System.Console.WriteLine("Print 1-255");
+            for (int i = 1; i < 256; i++)
+                Console.WriteLine(i);
+
+            System.Console.WriteLine("Print 1-100 % by 3 or 5 but not both");
+            for (int i = 1; i < 101; i++)
+                if((i%3 == 0 || i%5 == 0) && !(i%3 == 0 && i%5 == 0)) System.Console.WriteLine(i);
+
+            System.Console.WriteLine("Print 1-100 print Fizz if %3, Buzz if %5, or FizzBuzz if both");
+            for (int i = 1; i < 101; i++)
+            {
+                if(i%3 == 0 && i%5 == 0) System.Console.WriteLine("FizzBuzz");    
+                else if(i%3==0) System.Console.WriteLine("Fizz");
+                else if(i%5==0) System.Console.WriteLine("Buzz");   
+                else System.Console.WriteLine(i); 
+            }
+        }
+    }
+}
--- first_csharp/Program.cs
+++ first_csharp/Program.cs
@@ -12,6 +12,17 @@ namespace ConsoleApplication
 
             Console.WriteLine(cat.cat);
 
+            // loop from 1 to 5 including 5
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+            // loop from 1 to 5 excluding 5
+            for (int i = 1; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
         }
     }
 }
