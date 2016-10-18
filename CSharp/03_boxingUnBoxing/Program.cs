fc0401141ff71bbb884a5f4da998d369fa2a8951
fsheikh <farhanssheikh@gmail.com>
2016-10-17 21:24:44 -0500

    optionla assn complete

---
+++ CSharp/03_boxingUnBoxing/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<object> stuff = new List<object>();
+
+            stuff.Add("This is a string");
+            stuff.Add(0);
+            stuff.Add(true);
+
+            foreach (var item in stuff)
+            {
+                if(item is string) Console.WriteLine("A string");
+                else if(item is int) Console.WriteLine("A Number");
+                else if(item is bool) Console.WriteLine("A Boolean");
+            }
+
+            //To print out individually, we need to cast
+        }
+    }
+}
